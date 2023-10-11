c2998e35abaf687c859ef583bdcf08d63752476e
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-10-11 08:43:17 -0300

    Aprendendo sobre operadores aritiméticos

---
+++ Operadores_aritimetivos/operadores.py
@@ -0,0 +1,13 @@
+#n1 = int(input("Digite um numero: "))
+#n2 = int(input("Digite um segundo numero: "))
+#resultado = n1 + n2
+
+#print('a soma de {} + {} é igual a {}'.format(n1, n2, resultado))
+
+#Adicionando uma quebra de linha e formatando o resultado de uma divisão
+
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite um segundo numero: "))
+resultado = n1 / n2
+
+print('O resultado da divisão  de {} \n dividido por  {} \n é igual a {:.3f}'.format(n1, n2, resultado))
