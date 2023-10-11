9cea215a515aaac306f60f6c3bc20abd359055e6
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-10-11 08:29:26 -0300

    Adicionando tipos primitivos

---
+++ primitivos/primitivos.py
@@ -0,0 +1,20 @@
+#n = input('Digite um numero: ')
+#print(n)
+
+# Converter uma entrada para outro tipo primitivo integer
+#n = int(input('Digite um numero: '))
+#
+# print(type(n))
+
+# Converter uma entrada para outro tipo primitivo float
+#n = float(input('Digite um numero: '))
+#print(type(n))
+
+# Converter uma entrada para outro tipo primitivo boolean
+#n = bool(input('Digite um numero: '))
+#print(type(n))
+
+# realizando uma consulta de boleano com string
+#Quando eu digitar um numero retornar true e quando eu digitar uma letra retorna false
+n = input('Digite um numero: ')
+print(n.isnumeric())
