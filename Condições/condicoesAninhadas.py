364b53dd6911a924ae55f0b4aa28efa09d4ac59c
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-10-12 17:11:33 -0300

    aprendendo condicoes Aninhadas em python

---
+++ Condições/condicoesAninhadas.py
@@ -0,0 +1,9 @@
+print("Você tem 3 caminhas para ir escolha entre esquerda, direita ou frente")
+caminho = input("Digite o caminho escolhido: ")
+
+if caminho == "esquerda":
+    print("Esquerda foi o caminho escolhido")
+elif caminho == "direita":
+    print("Direita foi o caminho escolhido")
+else:
+    print("Frente foi o caminho escolhido")
