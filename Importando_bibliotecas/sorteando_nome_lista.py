6739acb806a85bd9063918aa47eea0aa47a67e02
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-10-12 15:57:21 -0300

    Aprendendo sobre importações

---
+++ Importando_bibliotecas/sorteando_nome_lista.py
@@ -0,0 +1,7 @@
+from random import choice
+
+listaDeFuncionarios = ["Vinicius", "Sulamita", "Juliane", "Castelo", "Eliane"]
+
+funcionarioSorteado = choice(listaDeFuncionarios)
+
+print("O funcionario sorteado foi : {}".format(funcionarioSorteado))
