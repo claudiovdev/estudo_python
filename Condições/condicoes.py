324a043ea21e2173fc0367100e83a754ac3aeb9d
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-10-12 16:59:13 -0300

    aprendendo concição if e else

---
+++ Condições/condicoes.py
@@ -0,0 +1,12 @@
+#Tarefa para praticar if e else perguntando a idade
+#idade = int(input("Digite sua idade: "))
+#if idade <30:
+#    print("Você é novo")
+#else:
+#    print("Você é velho")
+
+
+#Realizando if else em apenas uma linha
+idade = int(input("Digite sua idade: "))
+resultado = "Você é novo" if idade <= 30 else "Você é velho"
+print(resultado)
