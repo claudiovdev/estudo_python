2879a32be3801d62f58d69bf67133f0176f6459e
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-10-16 11:46:29 -0300

    enumerate

---
+++ collections_python/builtins/praticando.py
@@ -0,0 +1,12 @@
+numeros = [10,15,19,85,67,75,23,38]
+
+range(len(numeros))
+
+for i in range(len(numeros)):
+    print(i, numeros[i])
+
+list(enumerate(numeros))
+
+teste = list(enumerate(numeros))
+
+print(teste)
